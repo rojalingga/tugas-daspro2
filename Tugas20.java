c43c4989fbc744612a134c44282618a67bf5e98d
rojalingga <rojalingga2006@gmail.com>
2024-09-06 13:50:54 +0700

    ini biodata Rojalingga

---
+++ Tugas20.java
@@ -0,0 +1,16 @@
+public class Tugas20 {
+    
+    public static void main(String[] args) {
+        System.out.println("---------------------------------------------------------");
+        System.out.println("-- Data Mahasiswa Jurusan Teknologi Informasi POLINEMA --");
+        System.out.println("---------------------------------------------------------");
+        System.out.println("Nama    : Rojalingga Prana Abinaya");
+        System.out.println("NIM     : 244107060070");
+        System.out.println("Kelas   : SIB 1C");
+        System.out.println("No Absen: 20");
+        System.out.println("Prodi   : Sistem Informasi Bisnis");
+        System.out.println("Alamat  : Jl. Bandara Narita EM-14 Perum OMA VIEW Malang");
+        System.out.println("No HP   : 082335152353");
+        System.out.println("Hobi    : Otomotif");
+    }
+}
