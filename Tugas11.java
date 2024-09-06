1324720b362522fbc9b6d4c0875101a9ba0c7633
KrisnahadiJ <krisnahadij@gmail.com>
2024-09-06 17:04:07 +0700

    Ini Biodata Krisnahadi

---
+++ Tugas11.java
@@ -0,0 +1,15 @@
+public class Tugas11 {
+    public static void main(String[] args) {
+        System.out.println("-------------------------------------------------------");
+        System.out.println("--Data Mahasiswa Jurusan Teknologi Informasi Polinema--");
+        System.out.println("-------------------------------------------------------");
+        System.out.println("Nama         : Krisnahadi Jayawardana"); 
+        System.out.println("NIM          : 244107060001");
+        System.out.println("Kelas        : SIB C");
+        System.out.println("No Absen     : 11");
+        System.out.println("Prodi        : Sistem Informasi Bisnis");
+        System.out.println("Alamat       : Tuban, Jawatimur");
+        System.out.println("No HP        : 085143009254");
+        System.out.println("Hobi         : Hiking, Swiming, And Boxing");
+        }   
+}
