fa0b0b095e471b665bb9ae70fbc0cf634dd0aaba
daffaputra09 <daffaputra863@gmail.com>
2024-09-06 17:00:17 +0700

    add Daffa Java File

---
+++ Tugas06.java
@@ -0,0 +1,16 @@
+public class Tugas06 {
+    
+    public static void main(String[] args) {
+        System.out.println("---------------------------------------------------------");
+        System.out.println("-- Data Mahasiswa Jurusan Teknologi Informasi POLINEMA --");
+        System.out.println("---------------------------------------------------------");
+        System.out.println("Nama    : Daffa Putra Prasetya");
+        System.out.println("NIM     : 244107060088");
+        System.out.println("Kelas   : SIB 1C");
+        System.out.println("No Absen: 06");
+        System.out.println("Prodi   : Sistem Informasi Bisnis");
+        System.out.println("Alamat  : Jl. Tebo Tengah No. 50, Mulyorejo, Sukun, Kota Malang");
+        System.out.println("No HP   : 0895620103476");
+        System.out.println("Hobi    : Fotografi");
+    }
+}
